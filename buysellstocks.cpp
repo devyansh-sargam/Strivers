5eb63416a5315fe65f49054240bb9c44604949ab
Devyansh Sargam <devyanshsargamips@gmail.com>
2025-02-19 01:16:23 +0530

    buysellstocks

---
+++ buysellstocks.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+int maximumProfit(vector<int> &prices){
+    int mini = prices[0], profit = 0;
+    int n = prices.size();
+    for(int i = 1; i<n; i++){
+        int cost = prices[i]-mini;
+        profit = max(profit, cost);
+        mini = min(mini, prices[i]);
+    }
+    return profit;
+}
